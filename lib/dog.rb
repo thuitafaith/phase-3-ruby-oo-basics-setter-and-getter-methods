7311c5df395ea39d8fa60e8feef10a02b4f14818
thuitaf <faith.thuita@moringaschool.com>
2022-09-05 16:11:13 +0300

    commit tree cddc1f546d41e7e78d532da5d025260d40941817

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(name)
+        @name = name
+      end
+    
+      # getter method
+      def name
+        @name
+      end
+    def breed=(breed)
+        @breed = breed
+        
+    end
+    def breed
+        @breed
+    end
 
 end
